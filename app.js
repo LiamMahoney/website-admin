3718ad01c125f5df821e6490a968ab3419780e97
Liam Mahoney <liammahoney96@gmail.com>
2020-01-31 02:15:00 +0000

    limiting allow access control origin header

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ const logger = winston.createLogger({
 const app = express();
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    if (req.headers.origin && req.headers.origin.endsWith("liammahoney.dev")){
+        res.header("Access-Control-Allow-Origin", req.headers.origin);
+    }
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     if (req.method === "OPTIONS") {
         res.header("Access-Control-Allow-Methods", "PUT, POST, GET, PATCH, DELETE");
